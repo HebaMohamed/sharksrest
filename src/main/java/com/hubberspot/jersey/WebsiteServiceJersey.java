4c6b2317c8fa83110acaeb473efc60e41de8e73c
dell <dell@DESKTOP-D5F21LL>
2017-02-05 16:35:18 +0200

    test

--- src/main/java/com/hubberspot/jersey/WebsiteServiceJersey.java
+++ src/main/java/com/hubberspot/jersey/WebsiteServiceJersey.java
@@ -42,7 +42,7 @@ public class WebsiteServiceJersey {
     @Path("/go")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getgo(){
-        String output = "gooooooooooooooooooooo twst" ;
+        String output = "gooooooooooooooooooooo teeest" ;
         return Response.status(200).entity(output).build();
     }
    
