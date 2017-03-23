3aad51d59430808fdd74a0536da0bbb283a45d47
dell <hobahoby95@gmail.com>
2017-03-23 11:01:24 +0200

    all changes added

--- src/main/java/com/hubberspot/jersey/WebsiteServiceJersey.java
+++ src/main/java/com/hubberspot/jersey/WebsiteServiceJersey.java
@@ -804,8 +804,8 @@ public class WebsiteServiceJersey {
     ResultSet getDBResultSet(String query) throws Exception{
                     
         Class.forName("com.mysql.jdbc.Driver");            
-        conn = DriverManager.getConnection("jdbc:mysql://localhost/hebadb?" + "user=root&password=");
-        //conn = DriverManager.getConnection("jdbc:mysql://sql9.freesqldatabase.com/sql9159113?" + "user=sql9159113&password=wREVb19m3G");
+        //conn = DriverManager.getConnection("jdbc:mysql://localhost/hebadb?" + "user=root&password=");
+        conn = DriverManager.getConnection("jdbc:mysql://sql11.freesqldatabase.com/sql11164022?" + "user=sql11164022&password=GLj4H4TT5N");
         
         Statement st = conn.createStatement();
         ResultSet rs = st.executeQuery(query);
